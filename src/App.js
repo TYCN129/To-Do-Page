d333160a2d2066e0a9fc48dfd65abaed0d076d64
Vedant Bhandare <vedantabhandare@MacBook-Air.local>
2023-04-07 16:25:26 +0530

    Added quotes framework

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Main from './components/Main';
+import axios from 'axios';
 
 function App() {
+  const [quote, setQuote] = useState("");
+
+  const fetchQuote = async () => {
+    const apiData = await axios.get("https://catfact.ninja/fact");
+    setQuote(apiData.data.fact);
+    console.log(quote);
+  };
+
+  useEffect(() => {
+    fetchQuote();
+  }, []);
+
   return (
     <div className="App">
-      <Main />
+      <Main quote={quote} />
     </div>
   );
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import videoBG from '../assets/videoBG.mp4';
 
-const Main = async () => {
+const Main = (props) => {
     return (
         <div className='Main'>
             <video src={videoBG} autoPlay loop muted />
             <div className='overlay'></div>
             <div className='card'>
 
+            </div>
+            <div className='quote'>
+                <p>{props.quote}</p>
             </div>
         </div>
     );
