de60d29f3319f397983782f67eb103ef898c5fdb
Vedant Bhandare <vedantabhandare@MacBook-Air.local>
2023-04-07 19:10:12 +0530

    Added and aligned buttons

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,18 @@ import React from 'react';
 const Main = (props) => {
     return (
         <div className='Card'>
+            <div className='HeaderButtons'>
+                <div className='work'>
+                    <h3>Work</h3>
+                </div>
+                <div className='separator'><h3>|</h3></div>
+                <div className='home'>
+                    <h3>Home</h3>
+                </div>
+            </div>
+            <div className='TaskList'>
 
+            </div>
         </div>
     );
 }
