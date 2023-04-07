f0965ed3313c4d81d68ecedc087bcf08da376ea0
Vedant Bhandare <vedantabhandare@MacBook-Air.local>
2023-04-07 17:05:39 +0530

    Data fetched from RapidAPI. Not a 'responsive' widget yet

--- src/App.js
+++ src/App.js
@@ -6,11 +6,21 @@ import axios from 'axios';
 function App() {
   const [quote, setQuote] = useState("");
 
-  const fetchQuote = async () => {
-    const apiData = await axios.get("https://catfact.ninja/fact");
-    setQuote(apiData.data.fact);
-    console.log(quote);
+  const options = {
+    method: 'GET',
+    url: 'https://quotes-inspirational-quotes-motivational-quotes.p.rapidapi.com/quote',
+    params: {token: 'ipworld.info'},
+    headers: {
+      'X-RapidAPI-Key': '7e745b8494msh8a5a6dabc60009bp1263f8jsnbcd833ae56af',
+      'X-RapidAPI-Host': 'quotes-inspirational-quotes-motivational-quotes.p.rapidapi.com'
+    }
   };
+
+  const fetchQuote = async () => {
+    await axios.request(options).then((res) => {
+      setQuote(res.data.text);
+    })
+  }
 
   useEffect(() => {
     fetchQuote();
